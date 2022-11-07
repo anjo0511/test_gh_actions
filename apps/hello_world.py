fec8170a573773deff9ce16debcb6e928267389c
A.J <39155159+anjo0511@users.noreply.github.com>
2022-11-07 08:56:29 +0100

    Create hello_world.py

---
+++ apps/hello_world.py
@@ -0,0 +1,9 @@
+def hello_world(arg1, arg2, arg3, arg4, arg5, arg6):
+
+    test = [arg1, arg2, arg3, arg4, arg5, arg6, ]
+    
+    test, test2 = [arg1, arg2, arg3, arg4, arg5, arg6] ,[arg1, arg2, arg3, arg4, arg5, arg6]
+
+    print(
+        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa  aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    )
